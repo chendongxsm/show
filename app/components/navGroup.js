5102c06eb34dde98e0fda6f106be33b471e93b81
chendong <1570900756@qq.com>
2018-08-03 19:18:06 +0800

    updated the user page on 20180803

---
+++ app/components/navGroup.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import {Text, View, Image, StyleSheet} from 'react-native';
+
+
+export default class TaskList extends Component {
+  render() {
+    return (
+        
+      <View style={styles.wrap}>
+        {this.props.groupArr.map(item=><View style={styles.navWrap} key={item.title}>
+          <View style={styles.navLeft}>
+            <Image style={styles.navIcon} source={item.icon}></Image>
+            <Text style={styles.navText}>{item.title}</Text>
+          </View>
+          <View style={styles.navRight}>
+            <Text style={styles.navText}>{item.detail}</Text>
+            <Image style={styles.navArrow} source={require('../images/icon-arrow-right.png')}></Image>
+          </View>
+        </View>)}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  wrap: {
+    paddingLeft: 8,
+    backgroundColor: '#fff',
+  },
+  navWrap: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingTop: 8,
+    paddingBottom: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+    borderStyle: 'solid',
+    
+  },
+  navLeft: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  navRight: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  navIcon: {
+    width: 16,
+    height: 16,
+    marginRight: 10
+  },
+  navText: {
+    fontSize: 12
+  },
+  navArrow: {
+    width: 16,
+    height: 16,
+    marginRight: 5
+  }
+});
--- app/pages/user/user.js
+++ app/pages/user/user.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Image, StatusBar, ScrollView } from 'react-native';
 import Util from '../../utils/util'
-import { NavigationBar } from 'teaset'
+import NavGroup from '../../components/navGroup'
 
 
 
 export default class Task extends Component {
   render() {
-
+    let orderArr = [{
+      image: require('../../images/icon-1.png'),
+      title: '已申请',
+      type: 1 
+    },{
+      image: require('../../images/icon-2.png'),
+      title: '待完成',
+      type: 2 
+    },{
+      image: require('../../images/icon-3.png'),
+      title: '待确认',
+      type: 3 
+    },{
+      image: require('../../images/icon-4.png'),
+      title: '已完成',
+      type: 4 
+    },{
+      image: require('../../images/icon-5.png'),
+      title: '待评价',
+      type: 5 
+    },]
+    let navArr = [{
+      icon: require('../../images/icon-6.png'),
+      title: '会员认证',
+      type: 'click',
+      path: ''
+    },{
+      icon: require('../../images/icon-7.png'),
+      title: '收货地址',
+      type: 'click',
+      path: ''
+    },{
+      icon: require('../../images/icon-8.png'),
+      title: '商家入驻',
+      type: 'click',
+      path: ''
+    },{
+      icon: require('../../images/icon-9.png'),
+      title: '邀请有礼',
+      type: 'click',
+      path: ''
+    },{
+      icon: require('../../images/icon-10.png'),
+      title: '注销登录',
+      type: 'click',
+      path: ''
+    }]
     return (
       <View style={styles.container}>
         <View style={styles.topWrap}>
@@ -25,9 +71,26 @@ export default class Task extends Component {
             <Text style={styles.dataTitle}>我的积分</Text>
           </View>
         </View>
-        <View>
-          
+        <View style={styles.orderNav}>
+          <View style={styles.orderTitle}>
+            <View style={styles.orderTitleLeft}>
+              <Image style={styles.orderTitleLeftImg} source={require('../../images/icon-task-title.png')}></Image>
+              <Text style={styles.orderTitleText}>我的任务</Text>
+            </View>
+            <View style={styles.orderTitleRight}>
+              <Text style={styles.orderTitleText}>全部任务</Text>
+              <Image style={styles.orderTitleRightImg} source={require('../../images/icon-arrow-right.png')}></Image>
+              
+            </View>
+          </View>
+          <View style={styles.orderList}>
+            {orderArr.map(item=><View style={styles.orderWrap} key={item.title}>
+              <Image style={styles.orderImg} source={item.image}></Image> 
+              <Text style={styles.orderType}>{item.title}</Text>
+            </View>)}
+          </View>
         </View>
+        <NavGroup groupArr={navArr}/>
       </View>
     );
   }
@@ -44,7 +107,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     height: 150,
-    backgroundColor: '#5ba4f7',
+    backgroundColor: '#33a8de',
   },
   avatar: {
     width: 80,
@@ -66,11 +129,69 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    height: 80
+    height: 70,
+    backgroundColor: '#fff'
   },
   dataItem: {
     flex: 1,
     display: 'flex',
     alignItems: 'center'
-  }
+  },
+  orderNav: {
+    marginTop: 5,
+    marginBottom: 5,
+    backgroundColor: '#fff'
+  },
+  orderTitle: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+    borderStyle: 'solid',
+  },
+  orderTitleLeft: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  orderTitleLeftImg: {
+    width: 10,
+    height: 10,
+    marginRight: 6
+  },
+  orderTitleRight: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  orderTitleRightImg: {
+    width: 10,
+    height: 10
+  },
+  orderTitleText: {
+    fontSize: 10
+  },
+  orderList: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  orderWrap: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  orderImg: {
+    width: 22,
+    height: 22
+  },
+  orderType: {
+    marginTop: 4,
+    fontSize: 12
+  },
 });
